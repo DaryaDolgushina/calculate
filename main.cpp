12186d8791c1b7937a3e3c661d8bd22303c06cb2
DaryaDolgushina <96383827+DaryaDolgushina@users.noreply.github.com>
2022-12-05 14:28:20 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+# включить " mainwindow.h "
+# включить < QApplication>
+
+int main ( int argc, char *argv[])
+{
+    QApplication a (argc, argv);
+    Главное Окно w;
+    ж. показать ();
+
+    вернуть а. исполнить ();
+}
